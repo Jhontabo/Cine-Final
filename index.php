1ffb07a6a8a59fb1c8af569c3621e92b29324615
Jhon Tajumbina <82285377+Jhontabo@users.noreply.github.com>
2023-11-19 03:25:13 -0500

    corrigio error al agregar

--- index.php
+++ index.php
@@ -16,35 +16,40 @@ if (isset($_POST['buscar'])) {
         </div>
     </div>
 
-    <!-- Formulario de búsqueda -->
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card card-body buscador-peliculas">
-                <form action="index.php" method="post">
-                    <input type="text" name="titulo" id="titulo" onkeypress="buscarPeliculas()" class="input-busqueda">
-                    <input type="submit" value="Buscar" name="buscar" class="btn btn-buscar">
-                </form>
-            </div>
+<!-- Formulario de búsqueda mejorado -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-body buscador-peliculas">
+            <form action="index.php" method="post" class="form-inline">
+                <div class="input-group">
+                    <input type="text" name="titulo" id="titulo" onkeypress="buscarPeliculas()" class="form-control input-busqueda" placeholder="Buscar película ...">
+                    <button type="submit" name="buscar" class="btn btn-primary btn-buscar">
+                        <i class="fas fa-search"></i> Buscar
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
+ </div>
+
 
     <div class="btnNuevaP">
     <h6><a href="" class="btn btn-agregar" data-bs-toggle="modal" 
     data-bs-target="#exampleModal">Nueva película</a></h6>
     </div>
-    
 
-    <!-- Tabla de películas -->
+<!-- Tabla de películas -->
+<div class="container mt-4">
     <div class="row">
         <div class="col-md-12">
-            <div class="card card-body tabla-peliculas">
+            <div class="card card-body tabla-peliculas mx-auto">
                 <table class="table table-striped">
                     <thead>
                         <tr>
                             <th scope="col">Id</th>
                             <th scope="col">Título</th>
                             <th scope="col">Protagonista</th>
-                            <th scope="col">Género</th>
+                            <th scope="col"><i class="fas fa-film"></i>Género</th>
                             <th scope="col">Acciones</th>
                         </tr>
                     </thead>
@@ -72,57 +77,57 @@ if (isset($_POST['buscar'])) {
     </div>
 </div>
 
-<!-- Modal -->
+
+<!-- Modal mejorado -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Nueva pelicula</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal-header bg-primary text-white">
+        <h5 class="modal-title w-100" id="exampleModalLabel">Nueva película</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form action="agregarPelicula.php" method="post">
-            <div>
-                <label for="titulo">Titulo</label>
-                <input class="form-control" type="text" name="titulo">
-            </div>
-            <div>
-                <label for="protagonista">Protagonista</label>
-                <input class="form-control" type="text" name="protagonista">
-            </div>
-            <div>
-                <label for="horario">Horario</label>
-                <input class="form-control" type="text" name="horario">
-            </div>
-            <div>
-                <label for="genero">Genero</label>
-                <select name="genero" class="form-control" >
-                    <option value="1">Accion</option>
-                    <option value="2">Terror</option>
-                    <option value="3">Csi_Fi</option>
-                    <option value="4">Drama</option>
-                    <option value="5">Romance</option>
-                    <option value="6">Comedia</option>
-                    <option value="7">Infantil</option>
-                </select>
-            </div>
-            <div>
-                <label for="clasificacion">Clasificacion</label>
-                <select name="clasificacion" class="form-control" >
-                    <option value="1">Para todo publico</option>
-                    <option value="2">Mayores de 13 años</option>
-                    <option value="3">Mayores de 18 años</option>
-                    <option value="4">Menores de 13 años</option>
-                </select>           
-            </div>
-            <input class="form-control" type="submit" value="agregar Pelicula" name="agregar">
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div class="mb-3">
+            <label for="titulo" class="form-label">Título</label>
+            <input class="form-control" type="text" name="titulo" required>
+          </div>
+          <div class="mb-3">
+            <label for="protagonista" class="form-label">Protagonista</label>
+            <input class="form-control" type="text" name="protagonista" required>
+          </div>
+          <div class="mb-3">
+            <label for="horario" class="form-label">Horario</label>
+            <input class="form-control" type="text" name="horario" required>
+          </div>
+          <div class="mb-3">
+            <label for="genero" class="form-label">Género</label>
+            <select name="genero" class="form-control" required>
+              <option value="1">Acción</option>
+              <option value="2">Terror</option>
+              <option value="3">Ciencia Ficción</option>
+              <option value="4">Drama</option>
+              <option value="5">Romance</option>
+              <option value="6">Comedia</option>
+              <option value="7">Infantil</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="clasificacion" class="form-label">Clasificación</label>
+            <select name="clasificacion" class="form-control" required>
+              <option value="1">Para todo público</option>
+              <option value="2">Mayores de 13 años</option>
+              <option value="3">Mayores de 18 años</option>
+              <option value="4">Menores de 13 años</option>
+            </select>
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button class="btn btn-primary" name="agregar" type="submit">Agregar Película</button>
       </div>
     </div>
   </div>
 </div>
+
 
 <?php include "lib/footer.php"; ?>
--- lib/conexion.php
+++ lib/conexion.php
@@ -1,12 +1,21 @@
 <?php
-    //variables de conexion
-    $servername = "localhost";
-    $username = "root";
-    $password = "12345";
-    $database = "cine";
-    
-    //crea la cadena de conexion
-    $conexion = new mysqli($servername, $username, $password, $database)
-    or die("Fallo en la conexion".$conexion->connect_error);
-    
+// Variables de conexión
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$database = "cine";
+
+// Crear la cadena de conexión
+$conexion = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Fallo en la conexión: " . $conexion->connect_error);
+}
+
+// Establecer el juego de caracteres
+$conexion->set_charset("utf8");
+
+// Otras configuraciones si es necesario
+
 ?>
