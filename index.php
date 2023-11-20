a3fc9406aff8f38ddf47f75e8e051eac6db0fe0a
Jhon Tajumbina <82285377+Jhontabo@users.noreply.github.com>
2023-11-19 20:39:01 -0500

    se agrego el codigo sql para tener versiones a el repositorio

--- index.php
+++ index.php
@@ -45,17 +45,6 @@ if (isset($_POST['agregar'])) {
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Cartelera</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">Proximo</a>
-            </li>
-            
           </ul>
           <form class="d-flex" action="index.php" method="post" role="search">
             <input type="text" name="titulo" id="titulo" onkeypress="buscarPeliculas()" class="form-control input-busqueda" placeholder="Buscar película ...">
